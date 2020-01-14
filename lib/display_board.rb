fc07544c28f400d4887c50a07d66755afcacd6bb
William Lin <willlin1996@gmail.com>
2020-01-14 17:14:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,19 +7,3 @@ def display_board
  puts "-----------"
  puts "   |   |   "
  end
- 
-   it 'prints an entire board full of Os' do
-      
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-      
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-      
-       expect(rows[0]).to eq(" O | O | O ")
-       expect(rows[1]).to eq("-----------")
-       expect(rows[2]).to eq(" O | O | O ")
-       expect(rows[3]).to eq("-----------")
-       expect(rows[4]).to eq(" O | O | O ")
-       expect(true).to be(true)
-     end
-   end
