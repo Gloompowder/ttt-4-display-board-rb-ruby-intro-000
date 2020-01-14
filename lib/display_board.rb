15ac72849950ca911f60622a7b0a7155037276b4
William Lin <willlin1996@gmail.com>
2020-01-14 17:33:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
-require 'pry'
+# Define display_board that accepts a board and prints
+# out the current state.
 def display_board(board)
-  binding.pry
 #  puts "   |   |   "
 #  puts "-----------"
 #  puts "   |   |   "
